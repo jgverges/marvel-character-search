81e3f5b4eb8c3da1c59de6b0825f90400395cbe0
jgverges <jgverges@yahoo.es>
2025-02-08 18:39:19 +0100

    Add types for Vite environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_MARVEL_API_KEY: string;
+  readonly VITE_MARVEL_PUBLIC_API_KEY: string;
+  readonly VITE_MARVEL_PRIVATE_API_KEY: string;
   readonly VITE_MARVEL_API_BASE_URL: string;
 }
 
